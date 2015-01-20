9521be51142b53370599a98f84c30c2322f7ae54
Sharafasli <sharafasliev@gmail.com>
2015-01-19 20:43:41 -0500

    Initial Commit

---
+++ America3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  America3
+//
+//  Created by sharaf asliev on 1/19/15.
+//  Copyright (c) 2015 Sharaf Asliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
